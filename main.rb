b53ddd866cae97d37b28563bce8314fa9337f49c
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-06-19 01:24:56 +0530

    Disable case equality cop and fix some linter errors

--- main.rb
+++ main.rb
@@ -1,109 +1,93 @@
-  module Enumerable
-    def my_each
-      i = 0
-      while i < self.length
-      	yield(self[i])
-      	i+=1
-      	
-      end
-      self
+module Enumerable
+  def my_each
+    i = 0
+    while i < length
+      yield(self[i])
+      i += 1
+
     end
-
-    def my_each_with_index
-      i = 0
-      while i < self.length
-        yield(self[i], i)
-      	i+=1
-
-      end
-      self
-    end
-
-    def my_select
-      selected_item = []
-      self.my_each do |i|
-      	selected_item.push(i)
-      	
-      end
-      selected_item
-    end
-
-    def my_all?
-      self.my_each do |i|
-        if yield(i) == false
-          return false
-        	
-        end
-      end
-      true
-    end
-
-    def my_any?
-      self.my_each do |i|
-        if yield(i) == true
-          return true
-        	
-        end
-      end
-      false
-    end
-
-    def my_none?
-      self.my_each do |i|
-        if yield(i) == true
-          return false
-        	
-        end
-      end
-      true
-    end
-
-    def my_count?
-      count = 0
-      self.my_each do |i|
-        if yield(i) == true
-          count +=1
-        end
-      end
-      count
-    end
-
-    def my_map
-      items = []
-      self.my_each do |i|
-        proc.nil? ? items << proc.call(i) : items << yield(i)
-      
-      end
-      items 
-    end
-
-    def my_inject( first = self[0])
-      self.my_each do |i|
-        first = yield(first, i)
-      end
-      first
-    end
-
+    self
   end
 
-  arr = [2, 4, 5]
-  arr.my_each {|i| p i }
-  arr.my_each_with_index {|val, i| p "#{val} in index #{i}"}
-  p arr.my_select {|i| }
-  p arr.my_all? {|i| i > 3}
-  p arr.my_any? {|i| i > 3}
-  p arr.my_none? {|i| i > 9}
-  p arr.my_count? {|i| i > 3}
-  p arr.my_inject(0) {|total, i| total + i}
-  
+  def my_each_with_index
+    i = 0
+    while i < length
+      yield(self[i], i)
+      i += 1
 
-  def multiply_els(arr)
-    arr.my_inject(1) {|total, i| total * i}
+    end
+    self
   end
 
-  p multiply_els(arr)
+  def my_select
+    selected_item = []
+    my_each do |i|
+      selected_item.push(i)
+    end
+    selected_item
+  end
 
-  p arr.my_map { |i| i+=5 }
-  my_proc = proc.new {|i| i**3}
-  p arr.my_map(&my_proc)
-  
+  def my_all?
+    my_each do |i|
+      return false if yield(i) == false
+    end
+    true
+  end
+
+  def my_any?
+    my_each do |i|
+      return true if yield(i) == true
+    end
+    false
+  end
+
+  def my_none?
+    my_each do |i|
+      return false if yield(i) == true
+    end
+    true
+  end
+
+  def my_count?
+    count = 0
+    my_each do |i|
+      count += 1 if yield(i) == true
+    end
+    count
+  end
+
+  def my_map
+    items = []
+    my_each do |i|
+      proc.nil? ? items << proc.call(i) : items << yield(i)
+    end
+    items
+  end
+
+  def my_inject(first = self[0])
+    my_each do |i|
+      first = yield(first, i)
+    end
+    first
+  end
+end
+
+arr = [2, 4, 5]
+arr.my_each { |i| p i }
+arr.my_each_with_index { |val, i| p "#{val} in index #{i}" }
+p arr.my_select { |i| }
+p (arr.my_all? { |i| i > 3 })
+p (arr.my_any? { |i| i > 10 })
+p (arr.my_none? { |i| i > 9 })
+p (arr.my_count? { |i| i > 3 })
+p arr.my_inject(0) { |total, i| total + i }
+
+def multiply_els(arr)
+  arr.my_inject(1) { |total, i| total * i }
+end
+
+p multiply_els(arr)
+
+p (arr.my_map { |i| i + 5 })
+my_proc = proc.new { |i| i**3 }
+p arr.my_map(&my_proc)
