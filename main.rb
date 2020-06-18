a9c43909a8d35976c4880e801a93c874a25c3483
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-06-18 21:20:00 +0530

    Add the map and inject method

--- main.rb
+++ main.rb
@@ -67,6 +67,23 @@
       end
       count
     end
+
+    def my_map
+      items = []
+      self.my_each do |i|
+        proc.nil? ? items << proc.call(i) : items << yield(i)
+      
+      end
+      items 
+    end
+
+    def my_inject( first = self[0])
+      self.my_each do |i|
+        first = yield(first, i)
+      end
+      first
+    end
+
   end
 
   arr = [2, 4, 5]
@@ -77,3 +94,16 @@
   p arr.my_any? {|i| i > 3}
   p arr.my_none? {|i| i > 9}
   p arr.my_count? {|i| i > 3}
+  p arr.my_inject(0) {|total, i| total + i}
+  
+
+  def multiply_els(arr)
+    arr.my_inject(1) {|total, i| total * i}
+  end
+
+  p multiply_els(arr)
+
+  p arr.my_map { |i| i+=5 }
+  my_proc = proc.new {|i| i**3}
+  p arr.my_map(&my_proc)
+  
