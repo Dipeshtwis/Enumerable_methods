8caa17cab03e9df7054439c4c63d53aa93689f8c
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-07-02 18:45:28 +0530

    Add test for my_all

--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -13,7 +13,7 @@
       expect((number.my_each { |x| x * x })).to match(number)
 		end
 	end
-end
+
 describe '#my_each_with_index' do
 it 'enumerable when no block is given' do
 	expect((1..4).my_each).to be_instance_of(Enumerator)
@@ -43,5 +43,28 @@ it 'returns a new hash for which the block returns true' do
 	options = { likes: 10, post: 'NIce post' }
 	c = options[:likes]
 	expect(c).to eql(10)
+end
+end
+
+describe '#my_all' do
+  it 'should return true if no block given' do
+    expect([].my_all?).to eql(true)
+  end
+
+  it 'should return false if the block never return true' do
+    expect([1, true, nil].my_all?).to eql(false)
+  end
+
+  it 'should return false unless the value is the same as pattern' do
+    expect(["ram", "bear", "three"].my_all?(/take/)).to eql(false)
+  end
+
+  it 'should return false unless value matches the pattern' do
+    expect([1,2,3].my_all?{ |x| x > 2}).to eql(false)
+  end
+
+  it 'return false if the value is not equal to pattern' do
+    expect([3i, 4, 3].my_all?(Numeric)).to eql(true)
+  end
 end
 end
