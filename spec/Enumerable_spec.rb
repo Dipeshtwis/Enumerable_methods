1ffd4dc1c8f054c72a48117441c87d38498ae921
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-07-05 20:00:37 +0530

    Fix the rubocop errors

--- spec/Enumerable_spec.rb
+++ spec/Enumerable_spec.rb
@@ -6,104 +6,104 @@ describe Enumerable do
 
   describe '#my_each' do
     context 'when block is not given' do
-    it 'enumerable when no block is given' do
-      expect((1..4).my_each).to be_instance_of(Enumerator)
+      it 'enumerable when no block is given' do
+        expect((1..4).my_each).to be_instance_of(Enumerator)
+      end
     end
-  end
     context 'when an array is given' do
-    it 'should not modify an array' do
-      expect((number.my_each { |x| x * x })).to match(number)
+      it 'should not modify an array' do
+        expect((number.my_each { |x| x * x })).to match(number)
+      end
     end
-  end
   end
 
   describe '#my_each_with_index' do
     context 'when block is not given' do
-    it 'enumerable when no block is given' do
-      expect((1..4).my_each).to be_instance_of(Enumerator)
-    end
-  end
-  context 'when an array is given' do
-    it 'returns an item and its index' do
-      hash = {}
-      %w[mufin pizza cake].my_each_with_index do |item, index|
-        hash[item] = index
+      it 'enumerable when no block is given' do
+        expect((1..4).my_each).to be_instance_of(Enumerator)
       end
-      expect(hash).to include('mufin' => 0, 'pizza' => 1, 'cake' => 2)
     end
-  end
+    context 'when an array is given' do
+      it 'returns an item and its index' do
+        hash = {}
+        %w[mufin pizza cake].my_each_with_index do |item, index|
+          hash[item] = index
+        end
+        expect(hash).to include('mufin' => 0, 'pizza' => 1, 'cake' => 2)
+      end
+    end
   end
 
   describe '#my_select' do
     context 'when block is not given' do
-    it 'enumerable when no block is given' do
-      expect((1..4).my_select).to be_instance_of(Enumerator)
+      it 'enumerable when no block is given' do
+        expect((1..4).my_select).to be_instance_of(Enumerator)
+      end
     end
-  end
     context 'when array given' do
-    it 'should return new array with selected values' do
-      a = [1, 2, 3, 4, 5]
-      b = a.my_select(&:odd?)
-      expect(b).not_to eql(a)
-    end
+      it 'should return new array with selected values' do
+        a = [1, 2, 3, 4, 5]
+        b = a.my_select(&:odd?)
+        expect(b).not_to eql(a)
+      end
 
-    it 'returns a new hash for which the block returns true' do
-      options = { likes: 10, post: 'NIce post' }
-      c = options[:likes]
-      expect(c).to eql(10)
+      it 'returns a new hash for which the block returns true' do
+        options = { likes: 10, post: 'NIce post' }
+        c = options[:likes]
+        expect(c).to eql(10)
+      end
     end
-  end
   end
 
   describe '#my_all?' do
     context 'when block is not given' do
-    it 'should return true if no block given' do
-      expect([].my_all?).to eql(true)
+      it 'should return true if no block given' do
+        expect([].my_all?).to eql(true)
+      end
     end
-  end
 
     context 'when block is given' do
-    it 'should return false if the block never return true' do
-      expect([1, true, nil].my_all?).to eql(false)
-    end
+      it 'should return false if the block never return true' do
+        expect([1, true, nil].my_all?).to eql(false)
+      end
 
-    it 'should return false unless the value is the same as pattern' do
-      expect(string.my_all?(/take/)).to eql(false)
-    end
+      it 'should return false unless the value is the same as pattern' do
+        expect(string.my_all?(/take/)).to eql(false)
+      end
 
-    it 'should return false unless value matches the pattern' do
-      expect([1, 2, 3].my_all? { |x| x > 2 }).to eql(false)
-    end
+      it 'should return false unless value matches the pattern' do
+        expect([1, 2, 3].my_all? { |x| x > 2 }).to eql(false)
+      end
 
-    it 'return false if the value is not equal to pattern' do
-      expect([3i, 4, 3].my_all?(Numeric)).to eql(false)
-    end
+      it 'return false if the value is not equal to pattern' do
+        expect([3i, 4, 3].my_all?(Numeric)).to eql(false)
+      end
 
-    it 'return true' do
-      expect([3, 3, 3].my_all?(3)).to eql(true)
+      it 'return true' do
+        expect([3, 3, 3].my_all?(3)).to eql(true)
+      end
     end
-  end
   end
 
   describe '#my_any?' do
     context 'when block given' do
-    it 'should return true if the block provided is a value other than nil or false' do
-      expect([1, false, nil].my_any?(Integer)).to eql(true)
-    end
+      it 'should return true if the block provided is a value other than nil or false' do
+        expect([1, false, nil].my_any?(Integer)).to eql(true)
+      end
 
-    it 'return false if the block did not return true' do
-      expect(string.my_any?(/float/)).to eql(false)
-    end
+      it 'return false if the block did not return true' do
+        expect(string.my_any?(/float/)).to eql(false)
+      end
 
-    it 'should return false unless the value is the same as pattern' do
-      expect([1, false, nil].my_any?).to eql(true)
-    end
+      it 'should return false unless the value is the same as pattern' do
+        expect([1, false, nil].my_any?).to eql(true)
+      end
 
-    it 'should return true unless value matches the pattern' do
-      expect([1, 2, 3].my_any? { |x| x > 2 }).to eql(true)
+      it 'should return true unless value matches the pattern' do
+        expect([1, 2, 3].my_any? { |x| x > 2 }).to eql(true)
+      end
     end
   end
-end
 
   describe '#my_none?' do
     it 'return true if the block never return true for all the element provided' do
@@ -158,23 +158,23 @@ end
 
   describe '#my_inject' do
     context 'when no block is given' do
-    it 'should return zero if no argument is given' do
-      expect([].my_inject).to eql(0)
-    end
+      it 'should return zero if no argument is given' do
+        expect([].my_inject).to eql(0)
+      end
 
-    it 'applay the symbol param to the array' do
+      it 'applay the symbol param to the array' do
         expect(number.my_inject(2, :-)).to eq(-8)
       end
-  end
-    
-    context 'when a block is given' do
-    it 'return the sum of the element in the array' do
-      expect(number.my_inject(0) { |total, x| total + x }).to eql(10)
     end
 
-    it 'return the largest element from the string' do
-      expect(string.my_inject { |memo, word| memo.size > word.size ? memo : word }).to eql('bear')
+    context 'when a block is given' do
+      it 'return the sum of the element in the array' do
+        expect(number.my_inject(0) { |total, x| total + x }).to eql(10)
+      end
+
+      it 'return the largest element from the string' do
+        expect(string.my_inject { |memo, word| memo.size > word.size ? memo : word }).to eql('bear')
+      end
     end
-  end
   end
 end
